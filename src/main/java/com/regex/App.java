e9a9573fd9ae7b54b3b22cde4babef78c81a5772
SergeiOkon <serge.chinaki@gmail.com>
2019-10-01 21:52:04 +0300

    Added logic in App and BookService

--- src/main/java/com/regex/App.java
+++ src/main/java/com/regex/App.java
@@ -1,4 +1,15 @@
 package com.regex;
 
+import com.regex.service.BookService;
+
 public class App {
+    public static void main(String[] args) {
+        runApp();
+    }
+
+    static void runApp() {
+        BookService.setBook("D:/_JAVA/EPAM/RegexLab/src/main/resources/Book.txt");
+        System.out.println("Type count of letters in words that start with consonant you want to delete:");
+        BookService.deleteByLengthWithConsonants();
+    }
 }
--- src/main/java/com/regex/service/BookService.java
+++ src/main/java/com/regex/service/BookService.java
@@ -1,13 +1,39 @@
 package com.regex.service;
 
-public class FilterService {
-    int userWordLength;
-    final String regex = "\\b(\\w+)\\b\\s*(?=.*\\b\\1\\b)";
-    final String bookText = "Goodbye bye bye world world world\n";
-    private String result;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
-    private void deleteFromText(int wordLength) {
-        result = bookText.replaceAll(regex, "");
+public class BookService {
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static String bookText;
+    private static int wordLength;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void setBook(String path) {
+        try {
+            bookText = new String(Files.readAllBytes(Paths.get(path)), "UTF-8");
+        } catch (IOException e) {
+
+        }
     }
-    public void deleteWordsStartedWith
+
+    private static void scanUserWordLength() {
+        try {
+            wordLength = Integer.parseInt(scanner.nextLine());
+            if (wordLength < 1) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Try again:");
+            deleteByLengthWithConsonants();
+        }
+    }
+
+    public static void deleteByLengthWithConsonants() {
+        scanUserWordLength();
+        System.out.println(bookText.replaceAll("(?i)\\b[b-df-hj-np-tv-z]\\w{" + (wordLength - 1) + "}\\b", ANSI_RED + "-" + ANSI_RESET));
+    }
 }
